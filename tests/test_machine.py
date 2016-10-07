e91ac8c5132f7876fdd2c27d3b52db78a7c76f36
Billy <billy.tang164@gmail.com>
2016-10-07 18:43:25 +1100

    added two tests to test_machine

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -28,3 +28,25 @@ def test_input_fail():
     tasks = [1, 2, 'a']
     with pytest.raises(ValueError):
         Machine(tasks)
+
+def test_add_success():
+    '''
+    Returns a list in the right order
+    '''
+    tasks = [1, 10]
+    job = 3
+    sorted_tasks = [1, 3, 10]
+    m = Machine(tasks)
+    m.add_job(job)
+    assert m.tasks == sorted_tasks
+
+def test_remove_success():
+    '''
+    Returns a list in the right order
+    '''
+    tasks = [1, 3, 10]
+    job = 3
+    sorted_tasks = [1, 10]
+    m = Machine(tasks)
+    m.remove_job(job)
+    assert m.tasks == sorted_tasks
